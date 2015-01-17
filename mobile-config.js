15b5d31a53cc6783c5219d2735800d3b45e16833
Mika Väisänen <mika.vaisanen@aalto.fi>
2015-01-17 02:43:56 +0200

    added icons

---
+++ mobile-config.js
@@ -0,0 +1,18 @@
+App.info({
+  name: 'Flashcards',
+  description: 'Memorize Javascript or other languages via spaced repetition software.',
+  version: '0.0.1'
+});
+
+App.icons({
+  'iphone': 'resources/icons/icon-60.png',
+  'iphone_2x': 'resources/icons/icon-60@2x.png'
+});
+
+App.launchScreens({
+  'iphone': 'resources/splash/Default~iphone.png',
+  'iphone_2x': 'resources/splash/Default@2x~iphone.png',
+  'iphone5': 'resources/splash/Default-568h@2x~iphone.png'
+});
+
+App.setPreference('StatusBarStyle', 'black-translucent');
