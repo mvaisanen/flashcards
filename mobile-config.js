c1d9c66c44f450e259e4d1c3b6f2b4e507d634ad
Mika Väisänen <mika.vaisanen@aalto.fi>
2015-02-03 23:06:43 +0200

    Added more icons

--- mobile-config.js
+++ mobile-config.js
@@ -6,13 +6,15 @@ App.info({
 
 App.icons({
   'iphone': 'resources/icons/icon-60.png',
-  'iphone_2x': 'resources/icons/icon-60@2x.png'
+  'iphone_2x': 'resources/icons/icon-60@2x.png',
+  'ipad': 'resources/icons/icon-76.png'
+  'ipad_2x': 'resources/icons/icon-76@2x.png'
 });
 
 App.launchScreens({
   'iphone': 'resources/splash/Default~iphone.png',
   'iphone_2x': 'resources/splash/Default@2x~iphone.png',
-  'iphone5': 'resources/splash/Default-568h@2x~iphone.png'
+  'iphone5': 'resources/splash/Default-568h@2x~iphone.png',
 });
 
 App.setPreference('StatusBarStyle', 'black-translucent');
